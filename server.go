02b59a6b4bc079059f0204023c9e2187aec50479
蒼時弦也 <elct9620@frost.tw>
2013-12-15 14:16:56 +0800

    Update# basic open badge issuer

--- server.go
+++ server.go
@@ -3,16 +3,111 @@ package main
 import(
   "github.com/codegangsta/martini"
   "net/http"
+  "time"
+  "encoding/json"
+  "encoding/hex"
+  "crypto/sha256"
 )
+
+// Data Struct
+type BadgeAssertion struct {
+  Uid string
+  Recipient IdentityObject
+  Badge string
+  Verify VerificationObject
+  IssuedOn int64
+}
+
+type IdentityObject struct {
+  Identity string
+  Type string
+  Hashed bool
+  Salt string
+}
+
+type VerificationObject struct {
+  Type string
+  URL string
+}
+
+type Badge struct {
+  Name string
+  Description string
+  Image string
+  Criteria string
+  Issuer string
+}
+
+type Issuer struct {
+  Name string
+  URL string
+}
+
+// Middware
+func jsonMiddleware(res http.ResponseWriter, req *http.Request) {
+  res.Header().Set("Content-Type", "application/json")
+}
+
+// Handlers
+func handleBadge(req *http.Request) string {
+  badge := &Badge{
+    Name: "Test",
+    Description: "A test badge",
+    Image: "http://" + req.Host + "/images/badge.png",
+    Criteria: "http://" + req.Host + "/badge.html",
+    Issuer: "http://" + req.Host + "/organization",
+  }
+  badgeJSON, err := json.Marshal(badge)
+  if err != nil {
+    return "{\"error\": \"JSON Parse error\"}"
+  }
+  return string(badgeJSON)
+}
+
+func handleBadgeDetial(res http.ResponseWriter) string {
+  res.Header().Set("Content-Type", "text/html")
+  return "Just for test..."
+}
+
+func handleAssertion(req *http.Request) string {
+  hash := sha256.New()
+  hash.Write([]byte("example@sitcon.org"))
+  md := hash.Sum([]byte("salt"))
+  identity := &IdentityObject{
+    Identity: "sha256$" + hex.EncodeToString(md),
+    Type: "email",
+    Hashed: true,
+    Salt: "salt",
+  }
+  verify := &VerificationObject{Type: "hosted", URL: "http://" + req.Host + "/assertion"}
+  issued := time.Now()
+  assertion := &BadgeAssertion{
+    Uid: "badge-1",
+    Recipient: *identity,
+    Badge: "http://" + req.Host + "/badge",
+    Verify: *verify,
+    IssuedOn: issued.Unix(),
+  }
+  assertionJSON, _ := json.Marshal(assertion)
+  return string(assertionJSON)
+}
+
+func handleIssuer() string {
+  issuer := &Issuer{Name: "Frost Studio", URL: "http://frost.tw"}
+  issuerJSON, _ := json.Marshal(issuer)
+  return string(issuerJSON)
+}
 
 func main() {
   m := martini.Classic()
+  m.Use(martini.Static("assets"))
+  m.Use(jsonMiddleware)
   m.Get("/", func () string {
     return "Hello World"
   })
-  m.Get("/badge", func (res http.ResponseWriter, req *http.Request) string {
-    return "Host: " + req.URL.Host
-  })
-  m.Use(martini.Static("assets"))
+  m.Get("/badge", handleBadge)
+  m.Get("/badge.html", handleBadgeDetial)
+  m.Get("/assertion", handleAssertion)
+  m.Get("/organization", handleIssuer)
   m.Run()
 }
