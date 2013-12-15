d040bcd572119190547e0b8c2901741fee35d4ea
蒼時弦也 <elct9620@frost.tw>
2013-12-15 13:01:37 +0800

    Update# add /badge and show host

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import(
   "github.com/codegangsta/martini"
+  "net/http"
 )
 
 func main() {
@@ -9,5 +10,9 @@ func main() {
   m.Get("/", func () string {
     return "Hello World"
   })
+  m.Get("/badge", func (res http.ResponseWriter, req *http.Request) string {
+    return "Host: " + req.URL.Host
+  })
+  m.Use(martini.Static("assets"))
   m.Run()
 }
